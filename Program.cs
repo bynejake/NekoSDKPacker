19ab877cd08006246536762bb4b810613ad034fb
bynejake <bynejake007@gmail.com>
2023-05-10 02:41:23 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace NekoSDKPacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine(
+                    "Usage:\n" +
+                    "   CommandLine:\n" +
+                    "       NekoSDKPacker [folder]\n" +
+                    "   Or:\n" +
+                    "       Simply drag folder into exe.");
+                WaitingForUser();
+                return;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Specified folder doesn't exist.");
+                WaitingForUser();
+                return;
+            }
+
+            try
+            {
+                ArchiveWriter.Create(args[0], args[0] + ".pak");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                WaitingForUser();
+            }
+        }
+
+        private static void WaitingForUser()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+﻿### NekoSDKPacker
+
+Create `NekoSDK` engine `PAK` archive.
+
+#### Usage
+
+CommandLine
+```
+NekoSDKPacker "e:\extract\cg"
+```
+
+OR
+
+```
+Simply drag folder into exe
+```
+
+#### Note
+
+This tool creates file with signature `NEKOPACK4A`. Please check your original file before use this tool.
+
+### Credits
+
+* [crskycode/NekoGameSDK_PackTool](https://github.com/crskycode/NekoGameSDK_PackTool)
