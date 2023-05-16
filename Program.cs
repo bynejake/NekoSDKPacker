d676f9a81f91c5efde8e05dd12f5a496468913c3
bynejake <bynejake007@gmail.com>
2023-05-16 22:18:20 +0800

    move localization string into msg.resx

--- Program.cs
+++ Program.cs
@@ -1,46 +1,17 @@
-﻿using System;
-using System.Globalization;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text;
 
 namespace NekoSDKPacker
 {
     class Program
     {
-        private static string[] MsgZH =
-        {
-            "用法:\n" +
-            "   命令行:\n" +
-            "       NekoSDKPacker [目录]\n" +
-            "   或者:\n" +
-            "       将文件夹拖放到exe上",
-            "目录不存在。",
-            "打包成功: {0}",
-            "请按任意键继续..."
-        };
-
-        private static string[] MsgEN =
-        {
-            "Usage:\n" +
-            "   CommandLine:\n" +
-            "       NekoSDKPacker [folder]\n" +
-            "   Or:\n" +
-            "       Simply drag folder into exe",
-            "Specified folder doesn't exist.",
-            "Pack successfully: {0}",
-            "Press any key to continue..."
-        };
-
-        private static string[] Msg = CultureInfo.InstalledUICulture.Name.StartsWith("zh") ? MsgZH : MsgEN;
-
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
             if (args.Length != 1)
             {
-                Console.WriteLine(Msg[0]);
+                Console.WriteLine(Msg.Usage);
                 WatingIfNecessary();
                 return;
             }
@@ -48,7 +19,7 @@ namespace NekoSDKPacker
             string folderPath = Path.TrimEndingDirectorySeparator(args[0]);
             if (!Directory.Exists(folderPath))
             {
-                Console.WriteLine(Msg[1]);
+                Console.WriteLine(Msg.Error);
                 WatingIfNecessary();
                 return;
             }
@@ -57,7 +28,7 @@ namespace NekoSDKPacker
             {
                 string filePath = folderPath + ".pak";
                 ArchiveWriter.Create(folderPath, filePath);
-                Console.WriteLine(Msg[2], filePath);
+                Console.WriteLine(Msg.Success, filePath);
             }
             catch (Exception e)
             {
@@ -74,7 +45,7 @@ namespace NekoSDKPacker
         {
             if (GetConsoleProcessList(new uint[1], 1) == 1)
             {
-                Console.WriteLine(Msg[3]);
+                Console.WriteLine(Msg.Waiting);
                 Console.ReadKey();
             }
         }
---
+++ res/Msg.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System.Resources;
+
+namespace NekoSDKPacker
+{
+    internal class Msg
+    {
+        private static ResourceManager resourceMananger;
+
+        private Msg()
+        {
+        }
+
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMananger, null))
+                {
+                    resourceMananger = new ResourceManager("NekoSDKPacker.res.Msg", typeof(Msg).Assembly);
+                }
+                return resourceMananger;
+            }
+        }
+
+        internal static string Error
+        {
+            get
+            {
+                return ResourceManager.GetString("Error");
+            }
+        }
+
+        internal static string Success
+        {
+            get
+            {
+                return ResourceManager.GetString("Success");
+            }
+        }
+
+        internal static string Usage
+        {
+            get
+            {
+                return ResourceManager.GetString("Usage");
+            }
+        }
+
+        internal static string Waiting
+        {
+            get
+            {
+                return ResourceManager.GetString("Waiting");
+            }
+        }
+    }
+}
